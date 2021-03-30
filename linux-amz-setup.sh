c201c71e7f328a6af00648f9fa743bb0aba42edd
PaulBeauvais <paul.beauvais@gmail.com>
2021-03-30 16:12:31 -0400

    updated edsctl install

--- linux-amz-setup.sh
+++ linux-amz-setup.sh
@@ -81,8 +81,8 @@ echo "============================================================"
 echo "==========================================================="
 echo "install eksctl"
 mkdir eksctl
-curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /eksctl
 cd eksctl
+curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar -xz -C .
 mv ./eksctl /usr/local/bin/eksctl
 cd ..
 rm -rf eksctl
